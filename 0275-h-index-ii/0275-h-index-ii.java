21a8c91b17cce0addd5f7cc861259599bafd7519
pandeyshivam21092005 <pandeyshivam21092005@gmail.com>
2026-07-30 11:36:06 +0530

    Time: 0 ms (100%), Space: 50.4 MB (28.18%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.java
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -4,7 +4,8 @@ class Solution {
       int lo=0;int hi=n-1;
       while(lo<=hi){
         int mid=lo+(hi-lo)/2;
-        if(citations[mid]>=n-mid) hi=mid-1;
+        if(citations[mid]==n-mid) return n-mid;
+        else if(citations[mid]>n-mid) hi=mid-1;
         else lo=mid+1;
       }
       return n-lo;  
